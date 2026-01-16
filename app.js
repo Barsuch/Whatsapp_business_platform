8136765f17c9999d40d2a984718e939be54ffd34
Barsuch <60139841+Barsuch@users.noreply.github.com>
2026-01-16 12:06:52 +0300

    Fix logging of webhook success length

--- app.js
+++ app.js
@@ -64,8 +64,8 @@ function post_to_server(postData) {
 app.post('/', (req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
   console.log(`\n\nWebhook received ${timestamp}\n`);
-  //var hooked = JSON.stringify(req.body, null, 2);
-  console.log("webhooked success length:"+req.body.length);
+  var hooked = JSON.stringify(req.body, null, 2);
+  console.log("webhooked success length:"+hooked.length);
   post_to_server(req.body);
   res.status(200).end();
 });
